eff2c50af9577e96b3fe9c124f99f09d6df4a774
Jinzhu <wosmvp@gmail.com>
2015-11-04 11:05:59 +0800

    Register activity views

--- activity.go
+++ activity.go
@@ -1,6 +1,10 @@
 package activity
 
 import (
+	"os"
+	"path"
+	"strings"
+
 	"github.com/jinzhu/gorm"
 	"github.com/qor/qor/admin"
 	"github.com/qor/qor/audited"
@@ -16,6 +20,12 @@ type Activity struct {
 }
 
 func RegisterActivityMeta(res *admin.Resource) {
+	for _, gopath := range strings.Split(os.Getenv("GOPATH"), ":") {
+		admin.RegisterViewPath(path.Join(gopath, "src/github.com/qor/activity/views"))
+	}
+
+	res.UseTheme("activities")
+
 	res.Meta(&admin.Meta{
 		Name: "Activities",
 		Type: "activities",
