3fbf2431becdebd6313d9e2a878c72d2f59f3971
Jinzhu <wosmvp@gmail.com>
2015-11-05 18:09:52 +0800

    Ajax post form

--- views/themes/activities/assets/javascripts/activities.js
+++ views/themes/activities/assets/javascripts/activities.js
@@ -1,1 +1,8 @@
-alert("s")
+$(document).ready(function() {
+  $(".qor-field [type=submit]").parents("form").submit(function(event) {
+    $.post($(this).attr("action"), $(this).serialize(), function(data) {
+      console.log(data)
+    })
+    return false;
+  });
+})
