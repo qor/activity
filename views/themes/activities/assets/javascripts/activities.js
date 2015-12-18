7dc8b27a17fc6a40efa607e6f01c369500632e4e
binku <binku87@gmail.com>
2015-12-18 17:00:15 +0800

    disable input when blur

--- views/themes/activities/assets/javascripts/activities.js
+++ views/themes/activities/assets/javascripts/activities.js
@@ -17,6 +17,7 @@ QorActivity = {
 
   bindingEvents : function() {
     this.$scoped.on("click", ".qor-js-activity-item .qor-js-activity-edit", this.makeNoteEditable);
+    $(".qor-js-activity-input input").blur(this.disableNote);
   },
 
   appendTabsToFormContainer : function() {
@@ -40,9 +41,22 @@ QorActivity = {
 
   makeNoteEditable : function() {
     var $item = $(this).parents(".qor-js-activity-item");
+    $item.addClass("is-edit");
     $item.find(".qor-js-activity-note").hide();
     $item.find(".qor-js-activity-input").show();
     $(this).hide();
     $item.find(".qor-js-activity-save").show();
+    $item.find(".qor-js-activity-input input").focus();
+  },
+
+  disableNote : function() {
+    setTimeout(function() {
+      var $editingItem = $(".qor-js-activity-item.is-edit");
+      $editingItem.remove("is-edit");
+      $editingItem.find(".qor-js-activity-note").show();
+      $editingItem.find(".qor-js-activity-input").hide();
+      $editingItem.find(".qor-js-activity-edit").show();
+      $editingItem.find(".qor-js-activity-save").hide();
+    }, 200);
   }
 }
